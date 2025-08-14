a619b05ea426ea41161ee90934db43a99ef7be7e
whdudwns7100 <whdudwns7100@naver.com>
2025-08-14 14:05:51 +0900

    add : 맹독 공격

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ public:
     int power = 10;
     std::string m_name;
 
+    int poison_power = 3;
+    int poison_count = 0;
+
     int getHealth(){
         return health;
     }
@@ -32,11 +35,34 @@ public:
         return m_name;
     }
 
+
     int hitDamage(int power){
         this->health -= power;
         return this->health;
     }
 
+    int getPoisonPower(){
+        return poison_power;
+    }
+
+    bool isPoison(){
+        if(poison_count > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    int skillPoison(){
+        poison_count += 5;
+        return poison_count;
+    }
+
+    int usePoison(){
+       return poison_count -= 1;
+    }
+
+  
     
 };
 
@@ -68,27 +94,43 @@ int main() {
 
     while(1){
         
-        std::cout << player1.getName() << "님! 행위를 고르시오 : (1 공격, 2. 파워 업(힘 5 증가))"  << std::endl;
+        if(player1.isPoison()){
+            std::cout << player1.getName() << " 맹독 발동 "  << player1.usePoison() <<"회 남았습니다."<< std::endl;
+            std::cout << player2.getName() << "의 남은 피 : "  << player2.hitDamage(player1.getPoisonPower()) << std::endl;
+        }
+
+        if(player2.isPoison()){
+            std::cout << player2.getName() << " 맹독 발동 "  << player2.usePoison() <<"회 남았습니다."<< std::endl;
+            std::cout << player1.getName() << "의 남은 피 : "  << player1.hitDamage(player2.getPoisonPower()) << std::endl;
+        }
+
+
+        std::cout << player1.getName() << "님! 행위를 고르시오 : (1 공격, 2. 파워 업(힘 5 증가), 3. 맹독 피해)"  << std::endl;
         std::cin >> command;
 
         if(command == 1){
             std::cout << "공격 ! player2의 남은 피 : " << player2.hitDamage(player1.getPower()) << std::endl;
         }else if(command == 2){
             std::cout << "파워 업 ! player1의 힘 : " << player1.upPower() << std::endl;
+        }else if(command == 3){
+            std::cout << "맹독 공격 시전! " << player1.skillPoison() << "회 사용됩니다. "<< std::endl;
         }
+
 
         if(isDie(player2.getHealth())){
             std::cout << "player1 winner" << std::endl;
             return 0;
         }
 
-        std::cout << player2.getName() << "님! 행위를 고르시오 : (1. 공격, 2. 파워 업(힘 5 증가))"  << std::endl;
+        std::cout << player2.getName() << "님! 행위를 고르시오 : (1. 공격, 2. 파워 업(힘 5 증가), 3. 맹독 피해 5회)"  << std::endl;
         std::cin >> command;
         
         if(command == 1){
             std::cout << "공격 ! player1의 남은 피 : " << player1.hitDamage(player2.getPower()) << std::endl;
         }else if(command == 2){
             std::cout << "파워 업 ! player2의 힘 : " << player2.upPower() << std::endl;
+        }else if(command == 3){
+            std::cout << "맹독 공격 시전!" << player2.skillPoison() << "회 사용됩니다. "<< std::endl;
         }
 
         if(isDie(player1.getHealth())){
